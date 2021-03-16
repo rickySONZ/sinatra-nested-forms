477fea5fa674566b175b36da44564dc33e4ea3d4
Ryan Erricson <ryanerricson@gmail.com>
2021-03-16 18:09:36 -0400

    commit tree 450501c22a3a4871c074505b704a0a2ffa962ac3

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+    end
 end
